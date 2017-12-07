b8c1ab9377e0d52c463f7bb1f06ec3290a6bbd66
attc-249-10 <attc-249-10@tri-c.edu>
2017-12-07 13:09:57 -0500

    poached ETW code Chapter 1

---
+++ 06_while-loop.py
@@ -0,0 +1,16 @@
+while True:
+    x=input("Enter a number to count to: ")
+    if x == 'q' or x == 'quit':
+        break
+
+    x=int(x)
+    y=1
+
+    while True:
+        print(y)
+        y=y+1
+        if y>x:
+            break
+        elif y==300:
+            print("Your number is too large. Stop it.")
+            break
